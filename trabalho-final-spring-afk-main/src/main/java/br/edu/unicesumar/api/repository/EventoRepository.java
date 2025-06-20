bbe1c6720cff63260094ade2c386c7d8153a7478
AndreyIndras <145608538+andreyindras@users.noreply.github.com>
2025-06-20 00:06:01 -0300

    Delete trabalho-final-spring-afk-main directory

--- trabalho-final-spring-afk-main/src/main/java/br/edu/unicesumar/api/repository/EventoRepository.java
+++
@@ -1,9 +0,0 @@
-package br.edu.unicesumar.api.repository;
-
-import br.edu.unicesumar.api.entity.Evento;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EventoRepository extends JpaRepository<Evento, Long> {
-}
--- trabalho-final-spring-afk-main/src/main/java/br/edu/unicesumar/api/repository/PalestranteRepository.java
+++
@@ -1,9 +0,0 @@
-package br.edu.unicesumar.api.repository;
-
-import br.edu.unicesumar.api.entity.Palestrante;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PalestranteRepository extends JpaRepository<Palestrante, Long> {
-}
