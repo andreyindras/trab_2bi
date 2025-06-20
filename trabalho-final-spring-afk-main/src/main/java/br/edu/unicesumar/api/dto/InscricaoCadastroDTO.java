d7f0dd0b41fb5119990ce7935956b4189f28d723
AndreyIndras <145608538+andreyindras@users.noreply.github.com>
2025-06-20 00:21:44 -0300

    Add files via upload

---
+++ trabalho-final-spring-afk-main/src/main/java/br/edu/unicesumar/api/dto/InscricaoCadastroDTO.java
@@ -0,0 +1,18 @@
+package br.edu.unicesumar.api.dto;
+
+public class InscricaoCadastroDTO {
+
+    private Long idAluno;
+
+    public InscricaoCadastroDTO() {
+
+    }
+
+    public Long getIdAluno() {
+        return idAluno;
+    }
+
+    public void setIdAluno(Long idAluno) {
+        this.idAluno = idAluno;
+    }
+}
